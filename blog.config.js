fe8ff59b0bb98661aabdd3617a96527c61c75127
Xians <github@xians.su>
2023-12-29 23:33:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,61 +2,61 @@
  * @type {import('~/types').BlogConfig}
  */
 const BLOG = {
-  title: 'yokinist.me',
-  author: 'yokinist',
-  email: 'yokinist@gmail.com',
-  link: 'https://yokinist.me',
-  externalHPLink: 'https://bento.me/yokinist',
-  description: '通り過ぎていく日々の解釈。思考の断片、近況報告など。言葉にして置いておく場所。',
-  lang: 'ja-JP', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: '慢叨 Slowmur | Xians Su',
+  author: 'Xians Su',
+  email: 'hi@xians.su',
+  link: 'https://slowmur.com',
+  externalHPLink: 'https://xians.su',
+  description: '不時嘮叨，盡量言之有物。',
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'dark', // ['light', 'dark'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#040808', // use hex value, don't forget '#'
+  darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   profileSlug: 'profile',
-  since: 2021, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://simple-og-image.vercel.app', // The link to generate OG image, don't end with a slash
   // detail: https://github.com/yokinist/og-image/blob/main/api/_lib/types.ts#L2-L12
-  socialLink: 'https://twitter.com/yokinist',
+  socialLink: 'https://xians.su',
   seo: {
-    keywords: ['yokinist'],
-    googleSiteVerification: '', // Remove the value or replace it with your own google site verification code
+    keywords: ['xians', 'Xians Su', 'slowmur', '慢叨', '興趣使然的分心學家'],
+    googleSiteVerification: '_1A8LqqSKVpDRvIVBJIzizKL9y5T7poSIDkc8TbS5m0', // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ackee', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '', // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: 'https://ackee.xians.su/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: 'https://ackee.xians.su', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: 'a531dcad-6197-4326-9abe-3f2d2d35b463' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-PJ2C55Q3CH', // e.g: G-XXXXXXXXXX
+      measurementId: '', // e.g: G-XXXXXXXXXX
     },
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
     provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: 'for-gitalk', // The repository of store comments
-      owner: 'yokinist',
-      clientID: 'f06077c5603d6099ec37',
-      clientSecret: process.env.GITALK_CLIENT_SECRET,
-      admin: ['yokinist'],
-      id: 'yokinist.me', // Ensure uniqueness and length less than 50
+      repo: '', // The repository of store comments
+      owner: '',
+      clientID: '',
+      clientSecret: '',
+      admin: [''],
+      id: '', // Ensure uniqueness and length less than 50
       distractionFreeMode: false,
     },
     utterancesConfig: {
       repo: '',
     },
     cusdisConfig: {
-      appId: '4c9aa1e3-623a-4773-a9ef-f345f484efe5', // data-app-id'
+      appId: '', // data-app-id'
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
     },

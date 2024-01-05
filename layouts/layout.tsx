ef8bd4a4ddbcec247dc2897a8c38fc11f6df2096
Xians <github@xians.su>
2024-01-05 13:50:39 +0800

    Update layout.tsx

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -14,7 +14,7 @@ import formatDate from '~/lib/formatDate';
 import { useLocale } from '~/lib/i18n/locale';
 import { Post } from '~/types';
 import dynamic from 'next/dynamic';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface Link {
   id: string;

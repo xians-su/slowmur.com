cd1abb7d1484b75275f82eaa254220772cd1b58b
Xians <github@xians.su>
2024-01-05 10:53:43 +0800

    Update getOGImageURL.ts

--- lib/getOGImageURL.ts
+++ lib/getOGImageURL.ts
@@ -24,16 +24,8 @@ const convertObjToQueryString = (query: OGImageQuery): string => {
     .join('&');
 };
 
-export const getOGImageURL = ({ title, twitter, root }: GetOGImageUrlArgs): string => {
-  const defaultParams: OGImageQuery = {
-    //md: '1',
-    //fontSize: '96px',
-    //background: encodeURIComponent(BLOG.darkBackground),
-    //foreground: encodeURIComponent(BLOG.lightBackground),
-    siteTitle: encodeURIComponent(BLOG.title),
-    isTwitter: undefined,
-  };
-  const baseParams = `${BLOG.ogImageGenerateURL}/api/og?title=${encodeURIComponent(title)}&desc=Slowmur.com.png?`;
+export const getOGImageURL = ({ title, twitter, root }: GetOGImageUrlArgs): string ;
+  const baseParams = `${BLOG.ogImageGenerateURL}/api/og?title=${encodeURIComponent(title)}&desc=Slowmur.com`;
   if (twitter) {
     if (!root) {
       return (

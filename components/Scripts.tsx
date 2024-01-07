ac03874243ed27b63ea19303b89fffe691bfff21
Xians <github@xians.su>
2024-01-08 03:13:38 +0800

    Update Scripts.tsx

--- components/Scripts.tsx
+++ components/Scripts.tsx
@@ -14,14 +14,7 @@ export const Scripts: React.VFC = () => (
     {BLOG.analytics && BLOG.analytics.provider === 'ga' && (
       <>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.analytics.gaConfig.measurementId}`} />
-        <Script strategy="lazyOnload" id="ga">
-          {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${BLOG.analytics.gaConfig.measurementId}', {
-              page_path: window.location.pathname,
-            });`}
-        </Script>
+
       </>
     )}
     <noscript>

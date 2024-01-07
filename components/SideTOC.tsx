e2437f261af8220a29ceb60ae9cbaebe4ad461c6
Xians <github@xians.su>
2024-01-08 01:54:44 +0800

    Update SideTOC.tsx

--- components/SideTOC.tsx
+++ components/SideTOC.tsx
@@ -1,53 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
 
-// 定義 Link 物件的接口
-interface Link {
-  id: string;
-  title: string;
-  level: number;
-  active?: boolean;
-}
-
-// 定義組件 props 的接口
-interface SideTOCProps {
-  links: Link[];
-  posRef: React.RefObject<HTMLElement>;
-  anchorName: string;
-  minLevel: number;
-  visibleHeight?: number;
-  pause: boolean;
-}
-
-const SideTOC: React.FC<SideTOCProps> = ({
+const SideTOC = ({
   links: _links,
-  posRef,
   anchorName,
   minLevel,
   visibleHeight = 48,
-  pause,
 }) => {
-  const [show, setShow] = useState<boolean>(false);
-  const [anchor, setAnchor] = useState<number>(0);
-  const [links, setLinks] = useState<Link[]>(_links);
-  const [activeLink, setActiveLink] = useState<string | null>(null);
-  const tocRef = useRef<HTMLElement | null>(null);
+  const [show, setShow] = useState(false);
+  const [links, setLinks] = useState(_links);
+  const [activeLink, setActiveLink] = useState(null);
+  const tocRef = useRef();
 
-  const getActiveLinkID = (): string | undefined => {
-    return Array.from(document.querySelectorAll(`.${anchorName}`))
+  const getActiveLinkID = () =>
+    Array.from(document.querySelectorAll(`.${anchorName}`))
       .map((anchor) => ({
         top: anchor.getBoundingClientRect().top,
         id: anchor.id,
       }))
       .filter((item) => item.top <= 10)
       .sort((a, b) => b.top - a.top)[0]?.id;
-  };
 
   useEffect(() => {
-    if (pause) return;
-
     setLinks(
-      _links.reduce((prev: Link[], curr: Link) => {
+      _links.reduce((prev, curr) => {
         if (curr.id === activeLink) {
           prev.push({ ...curr, active: true });
         } else prev.push({ ...curr, active: false });
@@ -55,61 +30,58 @@ const SideTOC: React.FC<SideTOCProps> = ({
       }, [])
     );
 
-    const active = document.getElementById(`link-${activeLink}`)?.offsetTop || 0;
+    const active =
+      document.getElementById(`link-${activeLink}`)?.offsetTop + 40;
 
-    tocRef.current?.scrollTo({ top: active - 100, behavior: "smooth" });
-  }, [activeLink, pause, _links]);
+    tocRef.current &&
+      tocRef.current.scrollTo({ top: active - 100, behavior: "smooth" });
+  }, [activeLink, _links]);
 
-const handleScrollDirection = () => {
-  const activeId = getActiveLinkID();
-  setActiveLink(activeId !== undefined ? activeId : null); // 使用三元運算符來確保傳遞 string | null 而非 undefined
-  if (window.scrollY < visibleHeight) {
-    setShow(false);
-  } else {
-    setShow(true);
-  }
-};
-
-  const handleResize = () => {
-    setAnchor(posRef.current?.getBoundingClientRect().right || 0);
+  const handleScrollDirection = () => {
+    setActiveLink(getActiveLinkID());
+    if (window.scrollY < visibleHeight) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
   };
 
+  // const handleResize = () => {
+  //   setAnchor(posRef.current.getBoundingClientRect().right);
+  // };
+
   useEffect(() => {
-    handleResize();
+    // handleResize();
     window.addEventListener("scroll", handleScrollDirection);
-    window.addEventListener("resize", handleResize);
+    // window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("scroll", handleScrollDirection);
-      window.removeEventListener("resize", handleResize);
+      // window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   if (links.length === 0) return null;
 
-  return ReactDOM.createPortal(
-    <nav
+  return (
+    <div
       ref={tocRef}
-      className={`toc fixed top-24 bottom-8 w-[100%] origin-[0_0] overflow-y-auto border-gray-300 dark:border-gray-700 opacity-0 scale-0 lg:opacity-100 lg:scale-100 ${
-        show ? "scale-100" : "lg:scale-0 lg:opacity-0"
+      className={`toc fixed top-24 hidden lg:block bottom-8 w-[inherit] origin-[0_0] overflow-y-auto border-gray-300 dark:border-gray-700 ${
+        show ? "opacity-100" : "opacity-0"
       }`}
-      style={{
-        left: `${anchor + 40}px`,
-      }}
     >
-      <ul className="border-gray-300 dark:border-gray-700 border-l-[1px]">
+      <ul className="border-gray-300 dark:border-gray-700 border-l-[2px]">
         {links.map(({ id, title, level, active }) => (
           <li
             key={id}
             id={`link-${id}`}
             className={active ? "active-anchor-link" : ""}
-            style={{ marginLeft: `${level - minLevel}rem` }}
+            style={{ marginLeft: level - minLevel + "rem" }}
           >
             <a href={`#${id}`}>{title}</a>
           </li>
         ))}
       </ul>
-    </nav>,
-    document.body
+    </div>
   );
 };
 

beaa4db240c06139c690b86fe8e1ab2a30114092
Xians <github@xians.su>
2024-07-26 10:45:37 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer: React.VFC<Props> = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex flex-wrap justify-between align-baseline">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {BLOG.author}
           </p>{' '}
                       <a href="https://vercel.com">
 						Powered by
